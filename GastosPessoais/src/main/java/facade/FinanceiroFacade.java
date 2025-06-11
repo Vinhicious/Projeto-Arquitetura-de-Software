f73b1112e2c3588514db66532fb9f7899e55119f
Vinicios Trindade Costa <135274652+Vinhicious@users.noreply.github.com>
2025-06-10 23:24:52 -0300

    Update FinanceiroFacade.java

--- GastosPessoais/src/main/java/facade/FinanceiroFacade.java
+++ GastosPessoais/src/main/java/facade/FinanceiroFacade.java
@@ -1,6 +1,6 @@
 package facade;
 
-import command.RegistrarDespesaCommand;
+import db.DespesaDAO;
 import model.Despesa;
 import observer.Orcamento;
 import strategy.CategorizadorStrategy;
@@ -8,16 +8,18 @@ import strategy.CategorizadorStrategy;
 public class FinanceiroFacade {
     private CategorizadorStrategy estrategia;
     private Orcamento orcamento;
+    private DespesaDAO despesaDAO;
 
     public FinanceiroFacade(CategorizadorStrategy estrategia, Orcamento orcamento) {
         this.estrategia = estrategia;
         this.orcamento = orcamento;
+        this.despesaDAO = new DespesaDAO();
     }
 
     public void registrarDespesa(double valor, String descricao) {
         String categoria = estrategia.categorizar(new Despesa(valor, "", descricao));
         Despesa despesa = new Despesa(valor, categoria, descricao);
-        new RegistrarDespesaCommand(despesa).execute();
+        despesaDAO.salvar(despesa);
         orcamento.atualizarOrcamento(valor);
     }
 }
